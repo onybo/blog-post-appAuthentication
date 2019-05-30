175f67f4450b00fab5a1cd60a34a68c9ba7722ef
Olav Nybo <olavnybo@gmail.com>
2019-05-30 13:19:03 +0200

    initial

---
+++ cli/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace cli
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var provider = new AzureServiceTokenProvider("RunAs=Developer;DeveloperTool=AzureCLI");
+            try
+            {
+                var token = await provider.GetAccessTokenAsync("https://management.azure.com/");
+                Console.WriteLine($"token is : {token}");
+            }
+            catch (AzureServiceTokenProviderException ex)
+            {
+                Console.WriteLine($"Got exception. Have you logged in? : {ex.Message}");
+            }
+        }
+    }
+}
---
+++ clientSecret/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace servicePrincipal
+{
+    class Program
+    {
+        /// <param name="appId">Application Id of the service principal</param>
+        /// <param name="tenantId">Tenant id where the service principal is defined</param>
+        /// <param name="clientSecret">Client secret for the service principal</param>
+        static async Task Main(string appId, string tenantId, string clientSecret)
+        {
+            if (string.IsNullOrEmpty(appId) ||
+                string.IsNullOrEmpty(tenantId) ||
+                string.IsNullOrEmpty(clientSecret))
+                {
+                    Console.WriteLine("appId, tenantId and clientSecret must be specified");
+                    return;
+                }
+            var provider = new AzureServiceTokenProvider($"RunAs=App;AppId={appId};TenantId={tenantId};AppKey={clientSecret}");
+            try
+            {
+                var token = await provider.GetAccessTokenAsync("https://management.azure.com/");
+                Console.WriteLine($"token is : {token}");
+            }
+            catch (AzureServiceTokenProviderException ex)
+            {
+                Console.WriteLine($"Got exception. : {ex.Message}");
+            }
+        }
+    }
+}
